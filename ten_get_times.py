606e3af5c68b23c7dfea64ba2fd604c102c27452
Jackson Humphrey <jackson.s.humphrey@gmail.com>
2020-01-04 17:49:03 -0600

    Cleaned up ancillary files, added config file, and refactoredcode for pulling times from TEN.

--- ten_get_times.py
+++ ten_get_times.py
@@ -1,72 +1,161 @@
 #! /usr/bin/env python
-'''This scrapes every PR History page on the-elite.net,
-and concatenates all PR times into one table.'''
+'''This scrapes every PR History page on the-elite.net, and concatenates all PR times into one table.'''
 
-import csv, datetime, requests, sqlite3, time, tqdm
+import csv, itertools, requests, time, tqdm, yaml
 import pdb # pylint: disable = unused-import
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-from ten_classes import *
+from datetime import datetime
 
 LAST_CALL = time.time()
 CALL_INTERVAL = 15.0 # Minimum time between requests (seconds)
 
-PLAYERS_PAGE_URL = "https://rankings.the-elite.net/players"
-GAMES = {
-    "GoldenEye": {
-        "levels": [
-            "Dam",
-            "Facility",
-            "Runway",
-            "Surface 1",
-            "Bunker 1",
-            "Silo",
-            "Frigate",
-            "Surface 2",
-            "Bunker 2",
-            "Statue",
-            "Archives",
-            "Streets",
-            "Depot",
-            "Train",
-            "Jungle",
-            "Control",
-            "Caverns",
-            "Cradle",
-            "Aztec",
-            "Egypt"
-        ],
-        "difficulties": ["Agent", "Secret Agent", "00 Agent"],
-        "times_url_suffix": "/goldeneye/history"
-    },
-    "Perfect Dark": {
-        "levels": [
-            "dataDyne Central - Defection",
-            "dataDyne Research - Investigation",
-            "dataDyne Central - Extraction",
-            "Carrington Villa - Hostage One",
-            "Chicago - Stealth",
-            "G5 Building - Reconnaissance",
-            "Area 51 - Infiltration",
-            "Area 51 - Rescue",
-            "Area 51 - Escape",
-            "Air Base - Espionage",
-            "Air Force One - Antiterrorism",
-            "Crash Site - Confrontation",
-            "Pelagic II - Exploration",
-            "Deep Sea - Nullify Threat",
-            "Carrington Institute - Defense",
-            "Attack Ship - Covert Assault",
-            "Skedar Ruins - Battle Shrine",
-            "Mr. Blonde's Revenge",
-            "Maian SOS",
-            "WAR!",
-            "The Duel"
-        ],
-        "difficulties": ["Agent", "Special Agent", "Perfect Agent"],
-        "times_url_suffix": "/perfect-dark/history"
-    }
-}
+class Player:
+    def __init__(self, real_name, alias, game):
+        self.real_name = real_name
+        self.alias = alias
+        self.game = game
+
+        self.times = []
+
+        self.hex_code = None
+        self.times = {}
+        self.player_url = "https://rankings.the-elite.net/~" + alias.replace(" ", "+")
+        self.points = {}
+
+    def __str__(self):
+        return self.real_name + " / " + self.alias
+
+    def __repr__(self):
+        return self.real_name + " / " + self.alias
+
+    def recalculate_points(self):
+        self.points = sum([time.points for time in self.times])
+
+    def load_times(self, session):
+        '''This loads the Player's times with all of their PR times.'''
+        soup = request_soup(session, self.player_url + self.game.times_url_suffix)
+        try:
+            rows = [[cell.text.replace("\n", "") for cell in row.find_all("td")] for row in soup.find("table").find_all("tr")[1:]]
+        except AttributeError:
+            pass
+
+        self.times.extend([Time(
+            self,
+            self.game.find_stage(row[1], row[2]),
+            te_date_to_iso(row[0]),
+            row[4],
+            row[3]
+        ) for row in rows if row[0] != "Unknown" and row[3] != "N/A"])
+
+
+class Game:
+    def __init__(self, name, times_url_suffix, players_url, levels, difficulties):
+        self.name = name
+        self.times_url_suffix = times_url_suffix
+        self.players_url = players_url
+        self.stages = {Stage(level, difficulty, self) for level, difficulty in itertools.product(levels, difficulties)}
+
+        self.players = []
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name + ": stages = " + [repr(stage) for stage in self.stages]
+
+    def load_players(self, session):
+        '''This creates the set of Players by visiting the All Players page and parsing the table.'''
+        table = request_soup(session, self.players_url).find("table")
+        pdb.set_trace()
+        rows = [[cell.text.replace("\n", "") for cell in row.find_all("td")] for row in table.find_all("tr")[1:]]
+        self.players.extend([Player(row[1], row[0], self) for row in rows])
+
+    def find_stage(self, level_name, difficulty_name):
+        '''This returns the Stage for this game with the provided level and difficulty names.'''
+        try:
+            return [stage for stage in self.stages if stage.level.name == level_name and stage.difficulty.name == difficulty_name][0]
+        except IndexError:
+            raise LookupError("Stage " + level_name + " " + difficulty_name + " not found!")
+
+    def write_times(self):
+        '''This writes all the times out to a CSV.'''
+        with open("times.csv", "w") as outfile:
+            writer = csv.writer(outfile, delimiter = ",", quotechar = '"')
+            writer.writerow(["Player Name", "Player Alias", "Hex Code", "Date", "Game", "Level", "Difficulty", "Time", "System"])
+            for t in sorted(itertools.chain([player.times for player in self.players]), key = lambda t: t.date):
+                writer.writerow([
+                    t.player.real_name,
+                    t.player.alias,
+                    t.player.hex_code,
+                    t.date,
+                    t.stage.game,
+                    t.stage.level_name,
+                    t.stage.difficulty,
+                    t.time,
+                    t.system
+                ])
+
+
+class Stage:
+    def __init__(self, level_dict, difficulty_dict, game):
+        self.level = Level(level_dict["name"], level_dict["abbreviation"])
+        self.difficulty = Difficulty(difficulty_dict["name"])
+        self.game = game
+
+        self.times = set()
+
+    def __str__(self):
+        return self.level.name + " " + self.difficulty.name
+
+    def __repr__(self):
+        return self.level.name + " " + self.difficulty.name
+
+
+class Level:
+    def __init__(self, level_name, abbreviation):
+        self.name = level_name
+        self.abbreviation = abbreviation
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Difficulty:
+    def __init__(self, difficulty_name):
+        self.name = difficulty_name
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Time:
+    def __init__(self, player, stage, date, system, time_string):
+        self.player = player
+        self.stage = stage
+        self.date = date
+        self.system = system
+        self.time = time
+
+        self.player.times[self.stage.game].add(self)
+        self.stage.times.add(self)
+        self.points = 0
+
+    def __str__(self):
+        return str(self.player) + ": " + str(self.stage) + " " + self.time
+
+    def __repr__(self):
+        return str(self.player) + ": " + str(self.stage) + " " + self.time
+
+    def calculate_points(self):
+        pass
+
 
 def request_soup(session, url):
     '''Wraps the process of getting soup from a URL.'''
@@ -75,10 +164,11 @@ def request_soup(session, url):
         try:
             bucket_sleep()
             soup = BeautifulSoup(session.get(url).content, features = "lxml")
-        except (ConnectionError, TimeoutError) as e:    
+        except (ConnectionError, TimeoutError) as _:
             pass
-    
+
     return soup
+
 
 def bucket_sleep():
     '''Rate-limits requests, according to CALL_INTERVAL.'''
@@ -86,26 +176,11 @@ def bucket_sleep():
     time.sleep(max(CALL_INTERVAL - (time.time() - LAST_CALL), 0))
     LAST_CALL = time.time()
 
-def load_stages():
-    '''Sets up the Stages from the GAMES dict.'''
-    return {Stage(level, difficulty, game) for game, game_dict in GAMES.items() for level in game_dict["levels"] for difficulty in game_dict["difficulties"]}
 
 def te_date_to_iso(te_date):
     '''Converts the date format used on times pages to YYYY-MM-DD.'''
     return datetime.strptime(te_date, "%d %b %Y").strftime("%Y-%m-%d")
 
-def get_stage(stages, level, difficulty):
-    '''This returns the Stage with th specified level name and difficulty.'''
-    output = [stage for stage in stages if stage.level_name == level and stage.difficulty == difficulty]
-    if not output:
-        raise ValueError("Could not find stage \"" + level + " " + difficulty + "\"!")
-    return output[0]
-
-def get_players(session):
-    '''This creates the set of Players by visiting the All Players page and parsing the table.'''
-    table = request_soup(session, PLAYERS_PAGE_URL).find("table")
-    rows = [[cell.text.replace("\n", "") for cell in row.find_all("td")] for row in table.find_all("tr")[1:]]
-    return {Player(row[1], row[0], bool(row[2]), bool(row[3])) for row in rows}
 
 def get_hex_code(soup):
     '''This extracts the hex code from a player's PR history page.'''
@@ -114,56 +189,25 @@ def get_hex_code(soup):
     except TypeError:
         return "#000000"
 
-def get_times(session, players, stages):
-    '''This sweeps through every player's PR history page
-    and creates a set of all PR times posted on the site.'''
-    times = set()
-    for player in tqdm(players, desc = "Fetching times"):
-        for game, game_dict in GAMES.items():
-            if player.has_times(game):
-                soup = request_soup(session, player.player_url + game_dict["times_url_suffix"])
-                player.hex_code = get_hex_code(soup)
-                try:
-                    rows = [[cell.text.replace("\n", "") for cell in row.find_all("td")] for row in soup.find("table").find_all("tr")[1:]]
-                except AttributeError:
-                    continue
 
-                for row in rows:
-                    if all([row[0] != "Unknown", row[3] != "N/A"]):
-                        times.add(Time(
-                            player,
-                            get_stage(stages, row[1], row[2]),
-                            te_date_to_iso(row[0]),
-                            row[4],
-                            row[3]
-                        ))
+def set_up_game(config_filename, game_name):
+    with open(config_filename, "r") as infile:
+        game_yaml = [game for game in yaml.load(infile)["games"] if game["name"] == game_name][0]
+        return Game(game_name, game_yaml["times_url_suffix"], game_yaml["players_page"], game_yaml["levels"], game_yaml["difficulties"])
 
-    return times
-
-def write_times(times):
-    '''This writes all the times out to a CSV.'''
-    with open("times.csv", "w") as outfile:
-        writer = csv.writer(outfile, delimiter = ",", quotechar = '"')
-        writer.writerow(["Player Name", "Player Alias", "Hex Code", "Date", "Game", "Level", "Difficulty", "Time", "System"])
-        for each_time in sorted(times, key = lambda x: x.date):
-            writer.writerow([
-                each_time.player.real_name,
-                each_time.player.alias,
-                each_time.player.hex_code,
-                each_time.date,
-                each_time.stage.game,
-                each_time.stage.level_name,
-                each_time.stage.difficulty,
-                each_time.time,
-                each_time.system
-            ])
 
 def main():
     '''Execute top-level functionality'''
     session = requests.Session()
-    stages = load_stages()
-    players = get_players(session)
-    times = get_times(session, players, stages)
-    write_times(times)
 
-main()
+    goldeneye = set_up_game("games.yaml", "GoldenEye")
+    perfect_dark = set_up_game("games.yaml", "Perfect Dark")
+
+    for game in [goldeneye, perfect_dark]:
+        game.load_players(session)
+        for player in tqdm(game.players, desc = "Downloading times for " + game.name + " players"):
+            player.load_times(session)
+        game.write_times()
+
+if __name__ == "__main__":
+    main()
